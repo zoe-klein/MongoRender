884704796383333baec64d09eb8179245f55038a
Cris <48873935+ckorpio@users.noreply.github.com>
2023-03-15 12:52:00 -0500

    Add files via upload

---
+++ testmongo.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri = "mongodb+srv://classuser:cmps415class@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = client.db('ckmdb');
+    const movies = database.collection('cmps415');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { partID: '12345' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
