3e4090dcda89a771da1aabae8499500c9cf0e765
Cris <48873935+ckorpio@users.noreply.github.com>
2023-03-23 19:36:34 -0500

    Update and rename testmongo.js to testmongo2.js

--- testmongo2.js
+++ testmongo2.js
@@ -2,7 +2,34 @@ const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
 const uri = "mongodb+srv://classuser:cmps415class@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+// ------------------------------
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes will go here
+
+app.get('/', function(req, res) {
+  const myquery = req.query;
+  var outstring = 'Starting... ';
+  res.send(outstring);
+});
+
+
+
+
+app.get('/api/mongo', function(req, res) {
+
+
+
+
+//--------------------------
 const client = new MongoClient(uri);
 
 async function run() {
@@ -13,11 +40,14 @@ async function run() {
     // Query for a part that has partID '12345'
     const query = { partID: '12345' };
     const part = await parts.findOne(query);
-
     console.log(part);
+    res.send('Found ' + part);
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+});
+
 run().catch(console.dir);
