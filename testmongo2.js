9543d9ba5ceabd146f34d240284a3b281ad9efd6
Cris <48873935+ckorpio@users.noreply.github.com>
2023-03-23 20:09:03 -0500

    Update testmongo2.js

--- testmongo2.js
+++ testmongo2.js
@@ -2,11 +2,11 @@ const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
 const uri = "mongodb+srv://classuser:cmps415class@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
-// ------------------------------
+
+// --- This is the standard stuff to get it to work on the browser
 const express = require('express');
 const app = express();
 const port = 3000;
-
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
 
@@ -15,21 +15,15 @@ app.use(express.urlencoded({ extended: true }));
 
 // routes will go here
 
+// Default route:
 app.get('/', function(req, res) {
   const myquery = req.query;
   var outstring = 'Starting... ';
   res.send(outstring);
 });
 
-
-
-
+// Route to access database:
 app.get('/api/mongo', function(req, res) {
-
-
-
-
-//--------------------------
 const client = new MongoClient(uri);
 
 async function run() {
@@ -41,13 +35,13 @@ async function run() {
     const query = { partID: '12345' };
     const part = await parts.findOne(query);
     console.log(part);
-    res.send('Found ' + part);
+    res.send('Found this: ' + JSON.stringify(part));  //Use stringify to print a json
 
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-});
-
 run().catch(console.dir);
+
+});
