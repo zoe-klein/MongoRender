f63cf9a2528e8fb4debe900541ac456ec010abbc
Cris <48873935+ckorpio@users.noreply.github.com>
2023-03-25 08:02:59 -0500

    Update testmongo2.js

--- testmongo2.js
+++ testmongo2.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://classuser:cmps415class@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://<username>:<pasword>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
